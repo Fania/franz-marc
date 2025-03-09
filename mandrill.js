0202268f2a700c9d17864c4969e761d1fa5f1510
Fania Everitt <me@fania.eu>
2025-03-09 15:28:46 +0000

    first step, animating elements

--- mandrill.js
+++ mandrill.js
@@ -16,9 +16,18 @@ console.log(`And there are ${gradients.length} unique gradients.`);
 
 mandrill_svg.addEventListener("click", (ev) => {
   // console.log(ev);
-  // const elem = document.elementFromPoint(ev.offsetX, ev.offsetY);
+  const elem = document.elementFromPoint(ev.offsetX, ev.offsetY);
   // console.log(elem);
   console.log(`(${ev.offsetX}, ${ev.offsetY})`);
+  const item = document.createElementNS("http://www.w3.org/2000/svg", "animateTransform");
+  item.setAttribute('attributeName', 'transform');
+  item.setAttribute('attributeType', 'attributeType');
+  item.setAttribute('type', 'rotate');
+  item.setAttribute('from', `0 ${ev.offsetX} ${ev.offsetY}`);
+  item.setAttribute('to', `360 ${ev.offsetX} ${ev.offsetY}`);
+  item.setAttribute('dur', '10s');
+  item.setAttribute('repeatCount', '4');
+  elem.appendChild(item);
 });
 
 
