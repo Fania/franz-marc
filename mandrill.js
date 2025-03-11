076267d3895e1c83e80fec48cc8a55e35405af03
Fania Everitt <me@fania.eu>
2025-03-11 18:32:07 +0000

    success with rotation finally

--- mandrill.js
+++ mandrill.js
@@ -14,28 +14,60 @@ console.log(`And there are ${gradients.length} unique gradients.`);
 
 
 
-mandrill_svg.addEventListener("click", (ev) => {
-  // console.log(ev);
-  const elem = document.elementFromPoint(ev.offsetX, ev.offsetY);
-  // console.log(elem);
+mandrill_svg.addEventListener("click", async (ev) => {
+  // const elem = document.elementFromPoint(ev.offsetX, ev.offsetY);
+  // console.dir(elem);
   console.log(`(${ev.offsetX}, ${ev.offsetY})`);
-  const item = document.createElementNS("http://www.w3.org/2000/svg", "animateTransform");
-  item.setAttribute('attributeName', 'transform');
-  item.setAttribute('attributeType', 'attributeType');
-  item.setAttribute('type', 'rotate');
-  item.setAttribute('from', `0 ${ev.offsetX} ${ev.offsetY}`);
-  item.setAttribute('to', `360 ${ev.offsetX} ${ev.offsetY}`);
-  item.setAttribute('dur', '10s');
-  item.setAttribute('repeatCount', '4');
-  elem.appendChild(item);
+  // moveElement(ev,elem);
+  await moveElement(ev.target);
 });
 
 
 blocks.forEach(block => {
-  block.addEventListener("dblclick", (ev) => {
+  block.addEventListener("dblclick", async (ev) => {
     console.log(block.id);
+    // await moveElement(block);
     // console.log(ev);
     // const elem = document.elementFromPoint(ev.offsetX, ev.offsetY);
     // console.log(elem.id);
   });
+});
+
+
+
+
+async function moveElement(elem) {
+  // console.log(elem);
+  // console.dir(elem);
+  // const svgWidth = Math.ceil(elem.getBoundingClientRect().width);
+  // const svgHeight = Math.ceil(elem.getBoundingClientRect().height);
+  // const svgLeft = Math.ceil(elem.getBoundingClientRect().left);
+  // const svgTop = Math.ceil(elem.getBoundingClientRect().top);
+  // const svgRight = Math.ceil(elem.getBoundingClientRect().right);
+  // const svgBottom = Math.ceil(elem.getBoundingClientRect().bottom);
+  // console.log('svgWidth, svgHeight', svgWidth, svgHeight);
+  // console.log('svgLeft, svgTop', svgLeft, svgTop);
+  // console.log('svgRight, svgBottom', svgRight, svgBottom);
+  // const svgXcenter = svgLeft + (svgWidth / 2);
+  // const svgYcenter = svgTop + (svgHeight / 2);
+  // console.log(`Center: ${svgXcenter}, ${svgYcenter}`)
+
+  elem.setAttribute('style', `transform-origin: 50% 50%; transform-box: fill-box;`);
+  const item = document.createElementNS("http://www.w3.org/2000/svg", "animateTransform");
+  item.setAttribute('attributeName', 'transform');
+  item.setAttribute('type', 'rotate');
+  item.setAttribute('from', `0`);
+  item.setAttribute('to', `360`);
+  item.setAttribute('dur', '10s');
+  item.setAttribute('repeatCount', '4');
+  item.setAttribute('additive', 'sum');
+  elem.appendChild(item);
+}
+
+
+
+
+blocks.forEach(block => {
+  // console.log(block.id);
+  moveElement(block);
 });
