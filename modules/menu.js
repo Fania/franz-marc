71914d6e001de8a4a65083017324495253bcef46
Fania Everitt <me@fania.eu>
2025-03-20 16:01:54 +0000

    fix subOptions menu

--- modules/menu.js
+++ modules/menu.js
@@ -3,19 +3,25 @@ import { rotateElement, printColour, getColourSpeed } from "./rotate.js";
 
 const opts = document.querySelector('.options #buttons').children;
 const subOpts = document.querySelector('.subOptions').children;
+const subOptsNav = document.querySelector('.subOptions');
 const [...buttons] = document.getElementsByName('buttons');
 
 
-console.log(opts);
-console.log(subOpts);
+
 
 
 
 
 buttons.forEach(butt => {
-  butt.addEventListener('click', (event) => {
+  butt.addEventListener('change', (event) => {
+
+    const current = document.querySelector('[name="buttons"]:checked').value;
+    const currentElem = document.querySelector('[name="buttons"]:checked');
+    console.log(current);
+
     if(butt.id == 'original') {
       console.log('original');
+      subOptsNav.classList.remove('show');
       // stopAutoColours();
       // localStorage.clear();
       // saveColours(reh_defaults);
@@ -23,6 +29,7 @@ buttons.forEach(butt => {
     }
     if(butt.id == 'rotate') {
       console.log('rotate');
+      subOptsNav.classList.remove('show');
       // stopAutoColours();
       // localStorage.clear();
       // saveColours(reh_defaults);
@@ -30,6 +37,12 @@ buttons.forEach(butt => {
     }
     if(butt.id == 'paint') {
       console.log('paint');
+
+      if(current === 'paint') {
+        subOptsNav.classList.add('show');
+        subOpts[0].classList.add('show');
+        subOpts[1].classList.remove('show');
+      }
       // stopAutoColours();
       // clearCanvas();
       // clickListeners(handlePaint);
@@ -37,18 +50,23 @@ buttons.forEach(butt => {
     }
     if(butt.id == 'solids') {
       console.log('solids');
+      subOptsNav.classList.remove('show');
       // stopAutoColours();
       // mouseOverListeners(handleSolids);
       // updateColour('a_button_state', 'solids');
     }
     if(butt.id == 'gradientsR') {
       console.log('gradientsR');
+      subOptsNav.classList.remove('show');
       // stopAutoColours();
       // mouseOverListeners(handleGradients);
       // updateColour('a_button_state', 'gradientsR');
     }
     if(butt.id == 'automatic') {
       console.log('automatic');
+      subOptsNav.classList.add('show');
+      subOpts[0].classList.remove('show');
+      subOpts[1].classList.add('show');
       // startAutoColours();
       // updateColour('a_button_state', 'automatic');
     }
